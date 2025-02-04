77252ff98c0328fb70990c74e02c1324f1ecac21
gaelrf <gaelrey99@gmail.com>
2025-02-04 19:17:54 +0100

    conexion

---
+++ conexion.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = "localhost"; // o la IP de tu servidor MySQL
+$user = "root";
+$password = "";
+$database = "apptask";
+
+try {
+
+    // Crear la conexión con PDO
+    $conexion = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+
+    // Establecer el modo de error de PDO a excepción
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+
+    echo "Conexión fallida: " . $e->getMessage();
+
+}
