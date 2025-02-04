7581d38bbe22583a960ecc3d6ab0228b888bf12a
gaelrf <gaelrey99@gmail.com>
2025-02-04 19:22:13 +0100

    conection

--- conection.php
+++ conection.php
@@ -8,10 +8,10 @@ $database = "apptask";
 try {
 
     // Crear la conexión con PDO
-    $conexion = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+    $conection = new PDO("mysql:host=$host;dbname=$database", $user, $password);
 
     // Establecer el modo de error de PDO a excepción
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
 
