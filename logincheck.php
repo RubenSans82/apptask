9fc7fd69e23f62eb7eb4608068d4b6606193e7c4
gaelrf <gaelrey99@gmail.com>
2025-02-05 15:37:09 +0100

    login fixed

--- logincheck.php
+++ logincheck.php
@@ -2,16 +2,18 @@
 
 include 'conection.php';
 if (isset($_POST['user'])) {
-    $user = $_POST['user'];
+    $username = $_POST['user'];
     $password = $_POST['password'];
     $query = $conection->prepare("SELECT * FROM users WHERE username = :user");
-    $query->bindParam(':user', $user);
+    $query->bindParam(':user', $username);
     $query->execute();
     $user = $query->fetch(PDO::FETCH_ASSOC);
     if (password_verify($password, $user['password'])) {
         session_start();
-        $_SESSION['user'] = $user['users_id'];
+        $_SESSION['user_id'] = $user['users_id'];
+        $_SESSION['user'] = $username;
         header('Location: main.php');
+        exit;
     }
 
 }
