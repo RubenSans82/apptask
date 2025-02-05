9bca39b55e4b87af05932a60f1299e31380363b8
RubenSans82 <rubensans82@gmail.com>
2025-02-05 18:10:59 +0100

    some adjusts

--- index.php
+++ index.php
@@ -7,29 +7,33 @@
     <title>AppTask</title>
     <link rel="stylesheet" href="css/styles.css">
     <script src="js/script.js" defer></script>
+    <script src="https://cdn.lordicon.com/lordicon.js"></script>
+
 </head>
 
 <body>
     <header>
         <div class="header">
-            <a href="index.php"  class="logo"><img src="img/logo.png" alt="logo"></a>
+            <a href="index.php" class="logo"><img src="img/logo.png" alt="logo"></a>
             <div class="imgUser">
-                <img src="img/user.jpg" alt="user">
+                <lord-icon src="https://cdn.lordicon.com/iiudwewg.json" trigger="in" delay="500" state="in-reveal"
+                    colors="primary:#109121,secondary:#d1fad7" style="width:80px;height:80px">
+                </lord-icon>
+                </lord-icon>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="login.php">Iniciar Sesión</a></li>
-                <li><a href="register.php">Registrarse</a></li>
+                <div class="navButton"><li><a href="login.php">Iniciar Sesión</a></li></div>
+                <div class="navButton"><li><a href="register.php">Registrarse</a></li></div>
             </ul>
         </nav>
     </header>
     <main>
 
     </main>
-
-    <script src="js/script.js"></script>
 </body>
+
 <footer>
     <p>
         "Gael&Ruben © 2025"
