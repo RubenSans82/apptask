9384759e576307e23aa729ae36caad704febc9a2
gaelrf <gaelrey99@gmail.com>
2025-02-06 18:02:28 +0100

    register funciona

--- js/register.js
+++ js/register.js
@@ -3,13 +3,13 @@ const btnregister = document.getElementById("register")
 const password2 = document.getElementById("password2")
 
 function checkform() {
-    const aviso = document.getElementById("warning");
+    const aviso = document.getElementById("warning").querySelector('p');
     if (password.value == password2.value && password.value !== '') {
         btnregister.disabled = false
-        aviso.firstChild.style.display = "none";
+        aviso.style.display = "none";
     } else {
         btnregister.disabled = true
-        aviso.firstChild.style.display = "flex";
+        aviso.style.display = "block";
     }
     
 }
